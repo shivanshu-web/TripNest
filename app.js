63dae58f112543526c04316174ae1a22882bb7c1
shivanshu <sr4858244@gmail.com>
2026-01-20 20:22:18 +0530

    add new folder

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+
+const Listing = require("./models/listing.js");
+
+
+
+
+async function main() {
+     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+main().then(()=>{
+    console.log("connection successfully");
+}).catch(()=>{
+    console.log("err");
+
+
+});
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res) =>{
+    res.send("root");
+});
+
+// listing route 
+app.get("/listings",async(req,res)=>{
+ const allListings =  await Listing.find({});
+ 
+ res.render("listings/index.ejs",{allListings});
+});
+
+app.get("/listings/new",(req,res)=>{
+
+res.render("listings/new.ejs");
+});
+
+
+// show route
+
+app.get("/listings/:id", async(req,res)=>{
+
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  
+ 
+  res.render("listings/show.ejs",{listing});
+});
+
+// new route 
+
+app.post("/listings", async(req,res)=>{
+
+   
+    let newlisting = await new Listing(req.body.listing);
+    await newlisting.save();
+
+    res.redirect("listings");
+
+});
+
+
+
+
+// app.get("/listing",(req,res)=>{
+//   let sampleL = new Listing({
+//     title:"my house",
+//     description:"it is too beatiful ",
+//     price: 2000,
+//     location:"auraiya",
+//     country:"India",
+
+//   });
+
+//   sampleL.save().then((res)=>{
+//     console.log(res);
+//     console.log("save it ");
+//   }).catch((err)=>{
+//     console.log(err);
+//   })
+
+// });
+
+
+app.listen(8080,()=>{
+    console.log("port is listning on 8080");
+})
