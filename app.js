47f00f79f156f292854d879535c047b420889fc1
shivanshu <sr4858244@gmail.com>
2026-01-25 19:42:23 +0530

    add css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 
 const Listing = require("./models/listing.js");
+const ejsMate = require("ejs-mate");
 
 
 
@@ -26,6 +27,8 @@ app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/",(req,res) =>{
