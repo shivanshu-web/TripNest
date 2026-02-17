a11f57574bc6e5d54e8eb60c34d0caf4bddb138a
shivanshu <sr4858244@gmail.com>
2026-02-18 04:21:07 +0530

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require("method-override");
 
 const Listing = require("./models/listing.js");
 const ejsMate = require("ejs-mate");
-
+   
 
 
 
@@ -15,7 +15,7 @@ async function main() {
 
      await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
-
+   
 main().then(()=>{    
     console.log("connection successfully");
 }).catch(()=>{
