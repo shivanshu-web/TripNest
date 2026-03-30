c9041c82d05bd9ee500a9d5a90b1ae9f8ad9f05b
shivanshu <sr4858244@gmail.com>
2026-03-30 18:27:15 +0530

    addER

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.all(/.*/,(req,res,next)=>{
 
 app.use((err,req,res,next)=>{ 
     let {status = 500,message = "something went wrong"} = err;
-   res.status(status).send(message);
+    res.status(status).render("error",{err});
 
 });
 
