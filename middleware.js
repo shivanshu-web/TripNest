fbc8b6218e06f0e65796cd70a3932b8b72317ed3
shivanshu <sr4858244@gmail.com>
2026-06-16 22:10:33 +0530

    add

--- middleware.js
+++ middleware.js
@@ -2,10 +2,13 @@
 // this is for user authontication middleware;
 module.exports.isLoggedIn = (req,res,next)=>{
     
+    
     if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.orignalUrl;
         req.flash("error" , "you must be loged in to create listing");
        return res.redirect("/login");
     }
+    
     next();
     
 
